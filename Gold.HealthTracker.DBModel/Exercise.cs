4a174e91e98b9e7efdd13f7944dc01cbf1199124
Goldblatt <Thomas.Goldblatt@kik.de>
2024-03-13 10:05:58 +0100

    Added Exercise Class

---
+++ Gold.HealthTracker.DBModel/Exercise.cs
@@ -0,0 +1,9 @@
+﻿namespace Gold.HealthTracker.DBModel;
+
+public class Exercise
+{
+    public string ExerciseName { get; set; } = string.Empty;
+    public int Sets { get; set; }
+    public int Repetitions { get; set; }
+    public float ResistanceWeight { get; set; }
+}
