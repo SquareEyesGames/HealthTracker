46490bd8650f2ab5cee1212f14ca79bb5d6b6bf4
SquareEyesGames <t.schueren@gmx.net>
2024-03-28 20:37:01 +0100

    added CRUD for SleepRecords

--- Gold.HealthTracker.DTOModel/SleepRecordDTO.cs
+++ Gold.HealthTracker.DTOModel/SleepRecordDTO.cs
@@ -3,7 +3,18 @@
 public class SleepRecordDTO
 {
     public int Id { get; set; }
-    public DateOnly DateOfRecord { get; set; }
+    public int? PersonId { get; set; }
+    public DateTime DateOfRecord { get; set; }
+    public DateTime Bedtime { get; set; }
+    public DateTime WakeUpTime { get; set; }
+    public TimeSpan TimeAsleep => WakeUpTime - Bedtime;
+    public int? SleepQuality { get; set; }
+}
+
+public class CreateSleepRecordDTO
+{
+    public int? PersonId { get; set; }
+    public DateTime DateOfRecord { get; set; }
     public DateTime Bedtime { get; set; }
     public DateTime WakeUpTime { get; set; }
     public TimeSpan TimeAsleep => WakeUpTime - Bedtime;
--- Gold.HealthTracker.WebAPI/Controllers/SleepRecordController.cs
+++ Gold.HealthTracker.WebAPI/Controllers/SleepRecordController.cs
@@ -17,17 +17,141 @@ public class SleepRecordController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<SleepRecordDTO>>> GetSleepRecords()
+    [ProducesResponseType(200, Type = typeof(SleepRecordDTO))]
+    [ProducesResponseType(404)]
+    public IEnumerable<SleepRecordDTO?> GetSleepRecords()
     {
-        return await _context.SleepRecords.Select(sr => new SleepRecordDTO
+        if(_context.SleepRecords == null)
         {
-            Id = sr.Id,
-            DateOfRecord = sr.DateOfRecord,
-            Bedtime = sr.Bedtime,
-            WakeUpTime = sr.WakeUpTime,
-            SleepQuality = sr.SleepQuality
-        }).ToListAsync();
+            return (IEnumerable<SleepRecordDTO?>)NotFound();
+        }
+
+        return _context.SleepRecords.Select(sleepRecord => new SleepRecordDTO
+        {
+            Id = sleepRecord.Id,
+            PersonId = sleepRecord.Person == null ? 0 : sleepRecord.Person.Id,
+            DateOfRecord = sleepRecord.DateOfRecord.ToDateTime(TimeOnly.MinValue),
+            Bedtime = sleepRecord.Bedtime,
+            WakeUpTime = sleepRecord.WakeUpTime,
+            SleepQuality = sleepRecord.SleepQuality
+        });
     }
 
-    
+    [HttpGet("{id}")]
+    [ProducesResponseType(200, Type = typeof(SleepRecordDTO))]
+    [ProducesResponseType(404)]
+    public async Task<ActionResult> GetSleepRecord(int id)
+    {
+        SleepRecord? foundSleepRecord = await _context.SleepRecords.FindAsync(id);
+
+        if(foundSleepRecord == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(new SleepRecordDTO()
+        {
+            Id = foundSleepRecord.Id,
+            PersonId = foundSleepRecord.Person?.Id ?? 0,
+            DateOfRecord = foundSleepRecord.DateOfRecord.ToDateTime(TimeOnly.MinValue),
+            Bedtime = foundSleepRecord.Bedtime,
+            WakeUpTime = foundSleepRecord.WakeUpTime,
+            TimeAsleep = foundSleepRecord.TimeAsleep,
+            SleepQuality = foundSleepRecord.SleepQuality
+        });
+    }
+
+    [HttpPost]
+    [ProducesResponseType(201, Type = typeof(int))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult> CreateSleepRecord(CreateSleepRecordDTO newSleepRecordDTO)
+    {
+        if(!ModelState.IsValid)
+        {
+            return BadRequest();
+        }
+
+        SleepRecord newSleepRecord = new SleepRecord
+        {
+            Person = await _context.Persons.FindAsync(newSleepRecordDTO.PersonId),
+            DateOfRecord = DateOnly.FromDateTime(newSleepRecordDTO.DateOfRecord),
+            Bedtime = newSleepRecordDTO.Bedtime,
+            WakeUpTime = newSleepRecordDTO.WakeUpTime,
+            TimeAsleep = newSleepRecordDTO.TimeAsleep,
+            SleepQuality = newSleepRecordDTO.SleepQuality
+        };
+
+        await _context.SleepRecords.AddAsync(newSleepRecord);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetSleepRecord), new {id = newSleepRecord.Id}, newSleepRecord.Id);
+    }
+
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> UpdateSleepRecord(int id, CreateSleepRecordDTO sleepRecordDTO)
+    {
+        SleepRecord? sleepRecordToUpdate = await _context.SleepRecords.FindAsync(id);
+
+        if(sleepRecordToUpdate == null)
+        {
+            return NotFound();
+        }
+
+        sleepRecordToUpdate.Person = await _context.Persons.FindAsync(sleepRecordDTO.PersonId);
+        sleepRecordToUpdate.DateOfRecord = DateOnly.FromDateTime(sleepRecordDTO.DateOfRecord);
+        sleepRecordToUpdate.Bedtime = sleepRecordDTO.Bedtime;
+        sleepRecordToUpdate.WakeUpTime = sleepRecordDTO.WakeUpTime;
+        sleepRecordToUpdate.TimeAsleep = sleepRecordDTO.TimeAsleep;
+        sleepRecordToUpdate.SleepQuality = sleepRecordDTO.SleepQuality;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch(DbUpdateConcurrencyException)
+        {
+            if(!SleepRecordExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    private bool SleepRecordExists(int id)
+    {
+        return _context.SleepRecords.Any(e => e.Id == id);
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> DeleteSleepRecord(int id)
+    {
+        SleepRecord? sleepRecordToDelete = await _context.SleepRecords.FindAsync(id);
+
+        if(sleepRecordToDelete == null)
+        {
+            return NotFound();
+        }
+
+        try
+        {
+            _context.SleepRecords.Remove(sleepRecordToDelete);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        catch
+        {
+            return BadRequest();
+        }
+    }
 }
